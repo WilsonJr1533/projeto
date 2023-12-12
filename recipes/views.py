f6b0e7c0dc38d3f5dd954eb88cf11ae179843b37
Wilson Jr <magayver.10@hotmail.com>
2023-12-12 14:59:53 -0300

    initial

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
    
-def contato(request):
-    return HttpResponse('Contato')
-  
-def sobre(request):
-    return HttpResponse('Sobre')
